c7cd0f77db9c76fd034c3bde8704ced78b3bd7df
Michael's <mjlikre@gmail.com>
2020-12-12 13:14:51 -0600

    finished toggle buttons on the log page

---
+++ lib/toggle_button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class ToggleButton extends StatefulWidget {
+  @override
+  _ToggleButtonState createState() => _ToggleButtonState();
+}
+
+class _ToggleButtonState extends State<ToggleButton> {
+  List<bool> isSelected;
+
+  @override
+  void initState() {
+    isSelected = [true, false];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+      child: ToggleButtons(
+        borderColor: Colors.black,
+        fillColor: Colors.white,
+        borderWidth: 1,
+        selectedBorderColor: Color.fromRGBO(234, 197, 103, 1),
+        selectedColor: Colors.black,
+        borderRadius: BorderRadius.circular(12),
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 36.0, vertical: 0),
+            child: Text(
+              'Not Yet',
+              style: TextStyle(fontSize: 12, fontFamily: "Montserrat"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 36.0, vertical: 0),
+            child: Text(
+              'I did it!',
+              style: TextStyle(fontSize: 12, fontFamily: "Montserrat"),
+            ),
+          ),
+        ],
+        onPressed: (int index) {
+          setState(() {
+            for (int i = 0; i < isSelected.length; i++) {
+              isSelected[i] = i == index;
+            }
+          });
+        },
+        isSelected: isSelected,
+      ),
+    );
+  }
+}
