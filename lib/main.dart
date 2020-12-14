3130cca65b5629ca1b7ed6472b19c5eefc68186d
Michael <mgo518@berkeley.edu>
2020-12-14 14:28:07 -0800

    Completed first draft of backend changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
 import 'home.dart';
 
 void main() {
@@ -7,11 +9,31 @@ void main() {
 
 /// The entry point for bootstrapping the entire app.
 class WinterChallengeApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Klesis Winter Challenge',
-      home: Home(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return "Error";
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Klesis Winter Challenge',
+            home: Home(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return "Loading";
+      },
     );
   }
 }
